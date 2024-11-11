b1fb874b21d3373de1ba822fb425e17493c19b8c
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-11-11 16:54:00 +0330

    add tabel to cabin

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://wlklorwixidouvltfnvo.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indsa2xvcndpeGlkb3V2bHRmbnZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzNjE0MjAsImV4cCI6MjA0NTkzNzQyMH0.L9d6bLpYi7dX6quqDpYkpcXDRGFDoFOQo6KhGllqIGQ";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
