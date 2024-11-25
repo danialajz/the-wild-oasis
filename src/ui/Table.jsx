675bb4e35cb24cfad5256ed5e863b66adf4363d6
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-11-25 15:32:46 +0330

    add render props

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -85,7 +85,10 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No Data To Show</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 Table.Header = Header;
 Table.Body = Body;
 Table.Row = Row;
