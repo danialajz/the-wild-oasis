30960cdd144a547148d231376eb9e238dd4dd881
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-12-01 10:18:45 +0330

    add filter by status

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -3,7 +3,7 @@ import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
-import { useBookings } from "./UseBookings";
+import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
 
 function BookingTable() {
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,19 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  console.log(filter);
+
+  let query = supabase
     .from("bookings")
     .select(
-      "id, created_at , startDate, endDate , numNights ,numGuests, status,totalPrice,  cabins(name) ,guests(fullName,email)"
+      "id, created_at , startDate, endDate, numNights ,numGuests, status,totalPrice,  cabins(name) ,guests(fullName,email)"
     );
+
+  if (filter != null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+  const { data, error } = await query;
+
   if (error) {
     console.log(error);
     throw new Error("Booking could not be loaded");
