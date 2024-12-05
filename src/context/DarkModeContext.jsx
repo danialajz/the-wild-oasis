d47fbed3fa22641ce43c0617d5aa21313f84f965
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-12-05 09:44:53 +0330

    fix bug darkmode feture

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -6,15 +6,18 @@ const DarkmodeContext = createContext();
 function DarkModeProvider({ children }) {
   const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
 
-  useEffect(function () {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark-mode");
-      document.documentElement.classList.remove("light-mode");
-    } else {
-      document.documentElement.classList.add("light-mode");
-      document.documentElement.classList.remove("dark-mode");
-    }
-  }, [isDarkMode]);
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark-mode");
+        document.documentElement.classList.remove("light-mode");
+      } else {
+        document.documentElement.classList.add("light-mode");
+        document.documentElement.classList.remove("dark-mode");
+      }
+    },
+    [isDarkMode]
+  );
   function toggleDarkmode() {
     setIsDarkMode((isDark) => !isDark);
   }
